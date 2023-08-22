aa334c2ff6f3f84fab32e6653dc45acd5d2e3388
Zhi Rong <zrlin222@gmail.com>
2023-08-22 13:32:52 +0800

    add user system

--- resources/item.py
+++ resources/item.py
@@ -1,22 +1,24 @@
-from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from flask_jwt_extended import jwt_required
 from sqlalchemy.exc import SQLAlchemyError
 
 from db import db
 from models import ItemModel
 from schemas import ItemSchema, ItemUpdateSchema
 
-blp = Blueprint("Items", __name__, description="Operations on items")
+blp = Blueprint("Items", "items", description="Operations on items")
 
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
+    @jwt_required()
     @blp.response(200, ItemSchema)
     def get(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
         return item
-        
+
+    @jwt_required()
     def delete(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
         db.session.delete(item)
@@ -26,7 +28,8 @@ class Item(MethodView):
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
-        item  = ItemModel.query.get(item_id)
+        item = ItemModel.query.get(item_id)
+
         if item:
             item.price = item_data["price"]
             item.name = item_data["name"]
@@ -38,12 +41,15 @@ class Item(MethodView):
 
         return item
 
+
 @blp.route("/item")
 class ItemList(MethodView):
+    @jwt_required()
     @blp.response(200, ItemSchema(many=True))
     def get(self):
         return ItemModel.query.all()
-    
+
+    @jwt_required()
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):
@@ -53,6 +59,6 @@ class ItemList(MethodView):
             db.session.add(item)
             db.session.commit()
         except SQLAlchemyError:
-            abort(500, message="An error occured while inserting the item.")
+            abort(500, message="An error occurred while inserting the item.")
 
-        return item, 201
+        return item
--- resources/tag.py
+++ resources/tag.py
@@ -15,11 +15,13 @@ class TagsInStore(MethodView):
     def get(self, store_id):
         store = StoreModel.query.get_or_404(store_id)
 
-        return store.tags.all()
-    
+        return store.tags.all()  # lazy="dynamic" means 'tags' is a query
+
     @blp.arguments(TagSchema)
-    @blp.response(201,TagSchema)
+    @blp.response(201, TagSchema)
     def post(self, tag_data, store_id):
+        if TagModel.query.filter(TagModel.store_id == store_id, TagModel.name == tag_data["name"]).first():
+            abort(400, message="A tag with that name already exists in that store.")
         tag = TagModel(**tag_data, store_id=store_id)
 
         try:
@@ -28,9 +30,9 @@ class TagsInStore(MethodView):
         except SQLAlchemyError as e:
             abort(
                 500,
-                message=str(e)
+                message=str(e),
             )
-        
+
         return tag
 
 
@@ -48,9 +50,9 @@ class LinkTagsToItem(MethodView):
             db.session.commit()
         except SQLAlchemyError:
             abort(500, message="An error occurred while inserting the tag.")
-    
+
         return tag
-    
+
     @blp.response(200, TagAndItemSchema)
     def delete(self, item_id, tag_id):
         item = ItemModel.query.get_or_404(item_id)
@@ -60,10 +62,10 @@ class LinkTagsToItem(MethodView):
 
         try:
             db.session.add(item)
-            db.session.commit( )
+            db.session.commit()
         except SQLAlchemyError:
             abort(500, message="An error occurred while inserting the tag.")
-        
+
         return {"message": "Item removed from tag", "item": item, "tag": tag}
 
 
@@ -73,16 +75,16 @@ class Tag(MethodView):
     def get(self, tag_id):
         tag = TagModel.query.get_or_404(tag_id)
         return tag
-    
+
     @blp.response(
         202,
-        description="Deletes a taf if no item is tagged with it.",
-        example={"message": "Tag deleted."}
+        description="Deletes a tag if no item is tagged with it.",
+        example={"message": "Tag deleted."},
     )
     @blp.alt_response(404, description="Tag not found.")
     @blp.alt_response(
         400,
-        description="Returned if the tag is assigned to one or more items. In this vase the tag is not deleted."
+        description="Returned if the tag is assigned to one or more items. In this case, the tag is not deleted.",
     )
     def delete(self, tag_id):
         tag = TagModel.query.get_or_404(tag_id)
@@ -93,5 +95,5 @@ class Tag(MethodView):
             return {"message": "Tag deleted."}
         abort(
             400,
-            message="Could not delete tag. Make sure tag id not associated with any items, then try again."
+            message="Could not delete tag. Make sure tag is not associated with any items, then try again.",  # noqa: E501
         )
